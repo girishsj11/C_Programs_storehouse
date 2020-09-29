4603a9eee9f6c617f433e1b5efa37107b91b188b
girishsj11 <59494641+girishsj11@users.noreply.github.com>
2020-09-29 20:13:51 +0530

    Update Program_7.c

--- Program_7.c
+++ Program_7.c
@@ -6,6 +6,7 @@ void main()
     float f=3.10;
     char str[20];
     printf("\n %d %c %f",i,ch,f);
+    //printf is just presenting the things which is specified in it onto a cmd window
     sprintf(str,"\n %d %c %f ",i,ch,f);
     //sprintf stands for “String print”. Instead of printing on console, it store output on char 'str' which are specified in sprintf
     printf("%s",str);
