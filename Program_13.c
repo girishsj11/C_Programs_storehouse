1ee16978a405ea7109ad1badb62a773d3fcfacaf
girishsj <59494641+girishsj11@users.noreply.github.com>
2021-02-16 18:07:13 +0530

    Update Program_13.c

--- Program_13.c
+++ Program_13.c
@@ -4,14 +4,10 @@
 
 int main()
 {
-  int size=0,count=0;
-  
-  //To just to have a approximate size value for an char of array(string) to be declare below
-  printf("Enter the number of characters which you wanted to present on the string : \n");
-  scanf("%d",size);
-  
+  int count=0;
+ 
   //declaring & initializing the bunch of characters to the input char array s
-  char s[size];
+  char s[20];
   printf("Enter your favourite string here to give you the reverse of it : \n");
   scanf("%s",s);
   
@@ -20,10 +16,6 @@ int main()
     count++;
   }
   
-  if(count != size)
-  {
-    printf("The program is identified the that user entered character length is less than the size which he declared in begining ");
-  }
   for(int i = count-1; i>=0 ; i--)
     {
       printf("%c",s[i]);
